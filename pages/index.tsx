08ff62850763505cf7f93bd2f19712d7a841dccb
Logan Sparlin <logan.sparlin@gmail.com>
2021-04-05 12:44:37 -0500

    initial repo setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/about">About</Link>
+    </div>
+  );
+};
+
+export default Home;
