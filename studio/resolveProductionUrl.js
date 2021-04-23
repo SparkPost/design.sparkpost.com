b370999c65d62730de9daa2cf7384399c14bcfa0
Logan Sparlin <logan.sparlin@gmail.com>
2021-04-23 09:34:06 -0500

    UX-497 use correct preview url on production

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,5 +1,8 @@
 const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET; // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000';
+const projectUrl =
+  process.env.SANITY_STUDIO_ENV === 'production'
+    ? 'https://sparkpost-matchbox-v2.netlify.app'
+    : 'http://localhost:3000';
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&type=${document._type}&slug=${document.slug.current}`;
