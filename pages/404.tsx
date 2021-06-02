88e356bca946c0bd98cac24b0a485b7483081559
Jon Ambas <jon@jonambas.com>
2021-06-02 13:29:20 -0400

    UX-559 Try generic error page (#42)
    * Try with netlify toml redirect
    
    * Update redirect path
    
    * UX-559 try generic error page
    
    * try something else

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,61 +1,21 @@
 import { Box } from '@sparkpost/matchbox';
-import { getHomePage } from '@lib/api';
-import { usePreviewSubscription } from '@lib/sanity';
-import { Header } from '@components/header';
-import Footer from '@components/footer';
-import { SEO } from '@components/seo';
-import useSeo from '@hooks/useSeo';
+import Link from 'next/link';
 
-function NotFound({ data, preview }) {
-  const { data: pageData } = usePreviewSubscription(data?.query, {
-    params: { documentId: 'homePage' },
-    initialData: data?.pageData,
-    enabled: preview
-  });
-
-  const { site, seo } = pageData;
-
-  const { getSeoProps } = useSeo({
-    site: site?.seo,
-    page: seo
-  });
-
-  if (!pageData) {
-    return <div>Error</div>;
-  }
-
+function ErrorPage() {
   return (
-    <Box>
-      <SEO {...getSeoProps()} title="404 - Not Found" />
-      <Header title="Matchbox" items={site?.header?.menu?.items} />
-      <Box display="flex" height="70vh" alignItems="center" justifyContent="center">
-        <Box textAlign="center">
-          <Box fontSize="800" lineHeight="800">
-            404
-          </Box>
-          <Box fontSize="100" lineHeight="100">
-            Not Found
-          </Box>
+    <Box display="flex" height="100vh" alignItems="center" justifyContent="center">
+      <Box textAlign="center">
+        <Box fontSize="700" lineHeight="700">
+          <div>404</div>
+        </Box>
+        <Box fontSize="200">
+          <Link href="/">
+            <a>Go to home page</a>
+          </Link>
         </Box>
       </Box>
-      <Footer items={site?.footer?.menu?.items} />
     </Box>
   );
 }
 
-export async function getStaticProps({ preview = false }) {
-  const { data: pageData, query } = await getHomePage(preview);
-
-  return {
-    props: {
-      preview,
-      data: {
-        pageData,
-        query
-      }
-    },
-    revalidate: 10
-  };
-}
-
-export default NotFound;
+export default ErrorPage;
