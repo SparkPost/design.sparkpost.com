44c1262d34ab2442007b16f37d84715d670e1a94
Logan Sparlin <logan.sparlin@gmail.com>
2021-06-21 12:59:18 -0500

    UX-579 Icon list component for Iconography Page (#65)
    * UX-578 google analytics
    
    * UX-578 google analytics
    
    * UX-579 Icon List

---
+++ components/iconReferenceList/iconReferenceList.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import { Box, useCopyToClipboard, styles, TextField } from '@sparkpost/matchbox';
+import { Search } from '@sparkpost/matchbox-icons';
+import * as icons from '@sparkpost/matchbox-icons';
+import styled from 'styled-components';
+import css from '@styled-system/css';
+
+const StyledCopyButton = styled.button`
+  ${styles.buttonReset}
+  cursor: pointer;
+  display: block;
+
+  ${({ fontSize }) =>
+    css({
+      fontSize,
+      fontFamily: 'monospace',
+      pt: '400'
+    })}
+
+  &:hover {
+    ${css({
+      color: 'scheme.heavyAccent'
+    })}
+  }
+`;
+
+const StyledTextField = styled(TextField)`
+  border: none;
+  outline: none;
+`;
+
+function IconList({ items }) {
+  const { copy, copied } = useCopyToClipboard();
+  return items.map((icon, i) => {
+    const Icon = icons[icon];
+    return (
+      <Box
+        key={i}
+        gridColumn="span 1"
+        borderBottom="thick"
+        borderRight="thick"
+        width="calc(100% / 4)"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        py="600"
+      >
+        <Box textAlign="center">
+          <Icon size={40} />
+          <StyledCopyButton fontSize="50" onClick={() => copy(`<${icon} />`)}>
+            {copied ? 'Copied' : `<${icon} />`}
+          </StyledCopyButton>
+        </Box>
+      </Box>
+    );
+  });
+}
+
+function IconReferenceList() {
+  const [search, setSearch] = useState('');
+
+  const items = Object.keys(icons).filter((name) => {
+    return name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  return (
+    <Box width="100%">
+      <Box border="thick" mb="500">
+        <StyledTextField
+          onChange={(e) => setSearch(e.currentTarget.value)}
+          id="icon-search"
+          type="text"
+          placeholder="Search all icons"
+          suffix={<Search size="24" />}
+        />
+      </Box>
+      <Box display="flex" flexWrap="wrap" borderTop="thick" borderLeft="thick">
+        <IconList items={items} />
+        {items.length < 1 && (
+          <Box textAlign="center" width="100%" borderRight="thick" borderBottom="thick" p="400">
+            No Icons Found
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+}
+
+export default IconReferenceList;
