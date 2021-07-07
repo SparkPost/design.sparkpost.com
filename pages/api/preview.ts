05d665f8f6395f3252e8e08dca076a6f10aa6cff
Logan Sparlin <logan.sparlin@gmail.com>
2021-07-07 12:18:52 -0500

    UX-591 Fix redirect security vulnerability (#69)
    * Remove security vulnerability on preview redirect
    
    * UX-591 query the page based on slug and use that for the redirect
    
    * UX-591 rename to getPageSlug

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,12 +1,19 @@
-export default function handler(req, res) {
+import { previewClient } from '@lib/sanity';
+import { getPageSlug } from '@lib/api';
+
+export default async function handler(req, res) {
   const type = req.query?.type;
   const slug = req.query?.slug;
   const secret = req.query?.secret;
 
   // Bail if no secret or slug defined
-  if (secret !== process.env.NEXT_PUBLIC_SANITY_PREVIEW_SECRET || !type) {
+  if (secret !== process.env.NEXT_PUBLIC_SANITY_PREVIEW_SECRET || !type || !slug) {
     return res.status(401).json({ message: 'Invalid preview request' });
   }
+
+  const page = await previewClient.fetch(getPageSlug, {
+    slug
+  });
 
   // Enable Preview Mode by setting the cookies and passing the sanity token for fetching
   res.setPreviewData(
@@ -16,5 +23,6 @@ export default function handler(req, res) {
     }
   );
 
-  res.redirect(`${slug}`);
+  res.writeHead(307, { Location: `${page.slug}` });
+  res.end();
 }
