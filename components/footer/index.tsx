5ef44df2f211cccd5449a571d4726a7c5ed40b9c
Jon Ambas <jon@jonambas.com>
2021-07-09 14:14:25 -0400

    Add a note in footer about current matchbox version

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box } from '@sparkpost/matchbox';
 import styled from 'styled-components';
+import pkg from '../../package.json';
 
 type ItemProps = {
   slug: string;
@@ -24,6 +25,9 @@ function Footer(props: FooterProps): JSX.Element {
     <Box as="footer" border="thick" p="600" mt="-2px">
       <Ul>{items && items.map((item, i) => <Item {...item} key={i} />)}</Ul>
       <Box fontSize="100" color="scheme.fg" pt="800" mt="800">
+        This site is running on <strong>matchbox@{pkg.dependencies['@sparkpost/matchbox']}</strong>.
+      </Box>
+      <Box fontSize="100" color="scheme.fg">
         SparkPost © {new Date().getFullYear()}. All Rights Reserved.
       </Box>
     </Box>
